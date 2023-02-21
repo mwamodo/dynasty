7f25af643083cdb6003ef322c0331ddb95e92f7a
Mwamodo Erick <erick@mwamodo.com>
2023-02-21 22:10:09 +0300

    WIP

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,25 +10,15 @@ use Laravel\Jetstream\Features;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
@@ -37,11 +27,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return $this
-     */
     public function unverified(): static
     {
         return $this->state(function (array $attributes) {
@@ -51,11 +36,6 @@ class UserFactory extends Factory
         });
     }
 
-    /**
-     * Indicate that the user should have a personal team.
-     *
-     * @return $this
-     */
     public function withPersonalTeam(): static
     {
         if (! Features::hasTeamFeatures()) {

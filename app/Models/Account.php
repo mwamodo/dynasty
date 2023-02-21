b50b8fcda382321f4101a650bdae6d40d4ee68e7
Mwamodo Erick <erick@mwamodo.com>
2023-02-21 22:44:49 +0300

    WIP

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'has_overdraft' => 'boolean',
+    ];
 }
--- database/migrations/2023_02_21_185106_create_accounts_table.php
+++ database/migrations/2023_02_21_185106_create_accounts_table.php
@@ -10,12 +10,14 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
             $table->string('nickname')->nullable();
             $table->string('account_number')->nullable();
             $table->string('account_type')->nullable();
             $table->string('bank_name')->nullable();
             $table->integer('balance')->default(0);
+            $table->boolean('has_overdraft')->default(false);
             $table->timestamps();
         });
     }
